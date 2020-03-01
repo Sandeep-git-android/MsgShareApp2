b711e0ed6db16504c9004c2b3c34f8f0889b196e
Sandeep-git-android <sandeep.vamshi@yahoo.com>
2020-03-01 18:04:38 +0530

    Initial commit

---
+++ app/src/main/java/com/smartherd/sandeep/msgshareapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.smartherd.sandeep.msgshareapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity:AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+
+}
+//what to do from here on take3
