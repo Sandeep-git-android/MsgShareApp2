6c5befe5a567c3f112b16484dc2c8e8afaf9f5eb
Sandeep-git-android <sandeep.vamshi@yahoo.com>
2020-03-07 00:24:29 +0530

    Session two, Video two

--- app/src/main/java/com/smartherd/sandeep/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/smartherd/sandeep/msgshareapp/MainActivity.kt
@@ -2,14 +2,22 @@ package com.smartherd.sandeep.msgshareapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity:AppCompatActivity(){
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-    }
 
+        buttonShowToast.setOnClickListener {
+            Log.i("Main Activity", "ButtonClicked")
+
+            Toast.makeText(this,"Button was clicked",Toast.LENGTH_SHORT).show()
+        }
+    }
 }
 //what to do from here on take3
