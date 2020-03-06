a5ced798f2cea192eafb181caf2c54a34f5d6cc7
Sandeep-git-android <sandeep.vamshi@yahoo.com>
2020-03-07 01:00:47 +0530

    Section Two,Video three

--- app/src/main/java/com/smartherd/sandeep/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/smartherd/sandeep/msgshareapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.smartherd.sandeep.msgshareapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -18,6 +19,15 @@ class MainActivity:AppCompatActivity(){
 
             Toast.makeText(this,"Button was clicked",Toast.LENGTH_SHORT).show()
         }
+
+       btnSendMsgToNextActivity.setOnClickListener {
+           var message: String = editUserMsg.text.toString()
+           Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+
+           val intent = Intent(this,SecondActivity::class.java)//parameters of Intent(first_activity, second_Activity) //this,SecondActivity(java file)
+           startActivity(intent)
+
+       }
     }
 }
 //what to do from here on take3
